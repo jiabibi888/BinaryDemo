bd61e509fa044bcd97ad9973d37093d552c3ce61
Jabir <zjb11934@ly.com>
2021-12-08 18:02:09 +0800

    项目初始化

---
+++ BinaryDemo/BinaryDemo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  BinaryDemo
+//
+//  Created by Jabir-Zhang on 2021/12/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ BinaryDemo/BinaryDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BinaryDemo
+//
+//  Created by Jabir-Zhang on 2021/12/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
